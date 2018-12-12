69534e5f939832a83c71c837fceed8a8e2aaae6d
Balbri <nicolas.filine.lp2@gmail.com>
2018-12-12 09:39:23 +0100

    init

---
+++ session3Eval/src/main/java/co/simplon/session3Eval/Session3EvalApplication.java
@@ -0,0 +1,12 @@
+package co.simplon.session3Eval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Session3EvalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Session3EvalApplication.class, args);
+	}
+}
---
+++ session3Eval/src/test/java/co/simplon/session3Eval/Session3EvalApplicationTests.java
@@ -0,0 +1,16 @@
+package co.simplon.session3Eval;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Session3EvalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
